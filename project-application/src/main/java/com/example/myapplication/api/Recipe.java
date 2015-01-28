06cf07512f610590f9bad948b663a6507c677224
sakhtar <suhael.akhtar@despatch.net>
2015-01-28 17:32:48 +0000

    test

--- project-application/src/main/java/com/example/myapplication/api/Recipe.java
+++ project-application/src/main/java/com/example/myapplication/api/Recipe.java
@@ -1,16 +1,23 @@
 package com.example.myapplication.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by sakhtar on 22/12/2014.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Recipe implements Serializable {
 
     private String id;
     @NotNull
     private String title;
+    private List tags;
+    private List mealtimes;
+    private List ingredients;
 
     public Recipe(){
 
@@ -34,5 +41,29 @@ public class Recipe implements Serializable {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public List getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List getTags() {
+        return tags;
+    }
+
+    public void setTags(List tags) {
+        this.tags = tags;
+    }
+
+    public List getMealtimes() {
+        return mealtimes;
+    }
+
+    public void setMealtimes(List mealtimes) {
+        this.mealtimes = mealtimes;
     }
 }
--- project-application/src/main/resources/cookbook/scripts/controllers/recipeControllers.js
+++ project-application/src/main/resources/cookbook/scripts/controllers/recipeControllers.js
@@ -23,23 +23,58 @@ app.controller('NewRecipeCtrl', ['$scope', '$location', 'Recipe', 'ingredients',
         $scope.tags = tags;
         $scope.mealtimes = mealtimes;
 
-        $scope.selection = {
+        $scope.tselection = {
+            ids: {},
+            objects: []
+        };
+
+        $scope.mselection = {
+            ids: {},
+            objects: []
+        };
+
+        $scope.iselection = {
             ids: {},
             objects: []
         };
 
         $scope.save = function() {
+            $scope.recipe.ingredients = $scope.iselection.objects;
+            $scope.recipe.tags = $scope.tselection.objects;
+            $scope.recipe.mealtimes = $scope.mselection.objects;
             $scope.recipe.$save(function(recipe) {
                 $location.path('/recipe/' + recipe.id);
             });
+
         };
 
+        $scope.addIngredient = function (obj){
+            var index = $scope.ingredients.indexOf(obj)
+            $scope.ingredients.splice(index, 1);
+            $scope.iselection.objects.push(obj);
+        }
+
+        $scope.removeIngredient = function (obj){
+            var index = $scope.iselection.objects.indexOf(obj)
+            $scope.iselection.objects.splice(index, 1);
+            $scope.ingredients.push(obj);
+        }
+
         $scope.$watch(function() {
-            return $scope.selection.ids;
+            return $scope.tselection.ids;
         }, function(value) {
-            $scope.selection.objects = [];
-            angular.forEach($scope.selection.ids, function(v, k) {
-                v && $scope.selection.objects.push(getObjectById($scope.tags,k));
+            $scope.tselection.objects = [];
+            angular.forEach($scope.tselection.ids, function(v, k) {
+                v && $scope.tselection.objects.push(getObjectById($scope.tags,k));
+            });
+        }, true);
+
+        $scope.$watch(function() {
+            return $scope.mselection.ids;
+        }, function(value) {
+            $scope.mselection.objects = [];
+            angular.forEach($scope.mselection.ids, function(v, k) {
+                v && $scope.mselection.objects.push(getObjectById($scope.mealtimes,k));
             });
         }, true);
 
